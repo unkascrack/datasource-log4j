3402ce4ae880b7df3fdc396402b7c9e44ff2aa8c
carlos.alonso.gonzalez <carlos.alonso.gonzalez@b8452256-30ba-11de-8daa-cfecb5f7ff5b>
2012-07-20 07:23:50 +0000

    Añadido nuevo layout para p6spy, el cual elimina los intros de las consultas SQL

---
+++ src/main/java/es/map/sgtic/fw/log4j/P6spyLayout.java
@@ -0,0 +1,22 @@
+package es.map.sgtic.fw.log4j;
+
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.spi.LoggingEvent;
+
+public class P6spyLayout extends PatternLayout {
+
+    private static final String SALTO_LINEA = System.getProperty("line.separator");
+
+    public String format(final LoggingEvent event) {
+        String log = super.format(event);
+        if (log != null) {
+            log = log.replaceAll(SALTO_LINEA, " ");
+            log = log.replaceAll("\\t", " ");
+            while (log.contains("  ")) {
+                log = log.replaceAll("  ", " ");
+            }
+            log = log.trim() + SALTO_LINEA;
+        }
+        return log;
+    }
+}
